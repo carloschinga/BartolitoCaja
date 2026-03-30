613e4f3dd0462533614e619034b365370f776ff9
AaronCueva <aaron.cueva21@gmail.com>
2026-03-30 09:49:58 -0500

    AGREGADO ERRORES

--- src/main/java/com/bartolito/caja/service/CajaService.java
+++ src/main/java/com/bartolito/caja/service/CajaService.java
@@ -23,4 +23,8 @@ public class CajaService {
 		return cajaRepository.obtenerProductosVentasDescuadre(invnum);
 	}
 	
+	public List<Map<String, Object>> obtenerErroresVentasDescuadre(Integer invnum) {
+		return cajaRepository.obtenerErroresVentasDescuadre(invnum);
+	}
+	
 }
